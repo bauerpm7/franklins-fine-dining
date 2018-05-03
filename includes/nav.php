99ab88f4edee4c03c4512e81365b385cf65ad4cf
bauerpm <bauerpm@gmail.com>
2018-05-03 14:10:17 -0400

    First Commit

---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+<?php include('arrays.php') ?>
+
+<ul>
+  <?php 
+    foreach($navItems as $item) {
+      echo "<li><a href=\"$item[slug]\">$item[title]</a></li>";
+    }
+  ?>
+</ul>
---
+++ team.php
@@ -0,0 +1,13 @@
+<?php 
+  define("TITLE", "Team | Franklin's Fine Dining");
+  include('includes/header.php')
+ ?>
+
+  <div id = "team-members" class = 'cf'>
+    <h1>Our Team at Franklin's</h1>
+    <p>We're small but mighty.  Franklin's Fine Dining has been a family-owned and run business since the dirty thirties, and we're proud of it! When you get these three together, you never know what can happen. But you can count on one thing: <strong>The best food you've ever had. Ever</strong></p>
+  </div><!-- team members -->
+
+<?php 
+  include('includes/footer.php')
+  ?>
